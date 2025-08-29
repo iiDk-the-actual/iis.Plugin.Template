622d6aeb799354668ea81be0174e72ea45086529
iiDk-the-actual <54154105+iiDk-the-actual@users.noreply.github.com>
2025-08-29 17:19:43 -0400

    Update plugin template

--- Plugin.cs
+++ Plugin.cs
@@ -8,7 +8,7 @@ namespace StupidPlugin
     public class Plugin
     {
         /*
-            Plugin template used for ii's Stupid Menu 5.5.0+
+            Plugin template used for ii's Stupid Menu 6.9.0+
 
             If you've ever created a menu before, this should be easy:
                 + GetIndex(ButtonName); Gets a button's ButtonInfo
@@ -29,20 +29,26 @@ namespace StupidPlugin
         // You should put all of your adding of buttons / categories here
         public static void OnEnable()
         {
-            UnityEngine.Debug.Log("Plugin " + Name + " has been enabled!");
+            LogManager.Log("Plugin " + Name + " has been enabled!");
 
             int category = AddCategory("Plugin Mods");
-            AddButton(GetCategory("Main"), new ButtonInfo { buttonText = "Plugin Mods", method = delegate { buttonsType = category; pageNumber = 0; }, isTogglable = false, toolTip = "Brings you to a category for plugins." });
-            AddButton(category, new ButtonInfo { buttonText = "Exit Plugin Mods", method =() => Settings.ReturnToMain(), isTogglable = false, toolTip = "Returns you back to the main page." });
+            AddButton(GetCategory("Main"), new ButtonInfo { buttonText = "Plugin Mods", method =() => currentCategoryIndex = category, isTogglable = false, toolTip = "Brings you to a category for plugins." });
 
-            AddButton(category, new ButtonInfo { buttonText = "Right Trigger Fly <color=grey>[</color><color=green>RT</color><color=grey>]</color>", method = () => RightTriggerFly(), toolTip = "Returns you back to the main page." });
+            AddButtons(
+                category,
+                new ButtonInfo[]
+                {
+                    new ButtonInfo { buttonText = "Exit Plugin Mods", method =() => currentCategoryName = "Main", isTogglable = false, toolTip = "Returns you back to the main page." },
+                    new ButtonInfo { buttonText = "Right Trigger Fly <color=grey>[</color><color=green>RT</color><color=grey>]</color>", method = () => RightTriggerFly(), toolTip = "Returns you back to the main page." }
+                }
+            );
         }
 
         // This runs when the plugin stops, or when plugins are reloaded
         // You should put all of your removing of buttons / categories here
         public static void OnDisable()
         {
-            UnityEngine.Debug.Log("Plugin " + Name + " has been disabled!");
+            LogManager.Log("Plugin " + Name + " has been disabled!");
 
             RemoveCategory("Plugin Mods");
             RemoveButton(GetCategory("Main"), "Plugin Mods");
@@ -51,11 +57,11 @@ namespace StupidPlugin
         // This runs every frame before the mods
         public static void Update()
         {
-            // UnityEngine.Debug.Log(Time.time);
+            // LogManager.Log(Time.time);
         }
 
-        // This runs when the menu UI is open (togglable with backslash)
-        // I don't recommend using this as an update method
+        // This runs every frame when the menu UI is open (togglable with backslash)
+        // Don't use this like Update
         public static void OnGUI()
         {
             // GUI.Button(new Rect(10, 10, 200, 100), "Test Button");
@@ -66,8 +72,8 @@ namespace StupidPlugin
         {
             if (rightTrigger > 0.5f)
             {
-                GorillaLocomotion.Player.Instance.transform.position += GorillaTagger.Instance.headCollider.transform.forward * Time.deltaTime * flySpeed;
-                GorillaLocomotion.Player.Instance.GetComponent<Rigidbody>().velocity = Vector3.zero;
+                GorillaLocomotion.GTPlayer.Instance.transform.position += GorillaTagger.Instance.headCollider.transform.forward * Time.deltaTime * Movement.flySpeed;
+                GorillaLocomotion.GTPlayer.Instance.GetComponent<Rigidbody>().velocity = Vector3.zero;
             }
         }
     }
